f1ecc31d8a3b6b7bc979e1df1037d742b4a0086d
Daniel Lopes <danielvlopes@gmail.com>
2010-04-12 13:16:50 -0300

    refactoring

--- lib/delayed/mailer.rb
+++ lib/delayed/mailer.rb
@@ -4,18 +4,26 @@ module Delayed
   module Mailer
 
     def self.included(base)
+
       class << base
         alias_method :original_method_missing, :method_missing
-
-        def method_missing(method_symbol, *params)
-          if (/^deliver_([_a-z!]\w*)(?!\!)$/ =~ method_symbol.to_s)
-            send_later("#{method_symbol}!", *params)
-          else
-            original_method_missing(method_symbol, *params)
+      
+        def method_missing(method_symbol, *params, &block)
+          method_name = method_symbol.to_s
+      
+          case method_name
+            when /^deliver_([_a-z]\w*)$/
+              send_later(method_name, *params, &block)
+            when /^deliver_([_a-z]\w*)!$/
+              original_method_missing(method_name.gsub(/!$/,""), *params, &block)
+            else
+              original_method_missing(method_name, *params, &block)
           end
         end
-      end
-    end
+      
+      end #close eigen class
+
+    end #close hook
 
   end
 end
--- spec/delayed_mailer_spec.rb
+++ spec/delayed_mailer_spec.rb
@@ -20,31 +20,29 @@ describe AsynchTestMailer do
     Object.const_set 'RAILS_ENV', 'test' unless defined?(::RAILS_ENV)
   end
 
-  describe 'deliver_test_mail' do
-    before(:each) do
-      @emails = ActionMailer::Base.deliveries
-      @emails.clear
-      @params = 'noreply@autoki.de', 'joe@doe.com'
-      AsynchTestMailer.stub(:send_later)
-    end
+  before(:each) do
+    @emails = ActionMailer::Base.deliveries
+    @emails.clear
+    @params = 'noreply@myapp.com', 'jonh@doe.com'
+    AsynchTestMailer.stub(:send_later)
+  end
 
+  describe 'deliver_test_mail' do
     it 'should not deliver the email at this moment' do
       AsynchTestMailer.deliver_test_mail *@params
       @emails.size.should == 0
     end
 
     it 'should send deliver action to delayed job list' do
-      AsynchTestMailer.should_receive(:send_later).with('deliver_test_mail!', *@params)
+      AsynchTestMailer.should_receive(:send_later).with('deliver_test_mail', *@params)
       AsynchTestMailer.deliver_test_mail *@params
     end
   end
 
   describe 'deliver_test_mail!' do
-    it 'should deliver the mail' do
-      emails = ActionMailer::Base.deliveries
-      emails.clear
-      AsynchTestMailer.deliver_test_mail!('noreply@autoki.de', 'joe@doe.com')
-      emails.size.should == 1
+    it 'should deliver the mail now' do
+      AsynchTestMailer.deliver_test_mail! *@params
+      @emails.size.should == 1
     end
   end
 
